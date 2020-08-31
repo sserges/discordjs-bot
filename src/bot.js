21b5007c9a7191c427fe99ae20bd45d2a2654a8a
sserges <agounkeserge@gmail.com>
2020-08-31 11:26:58 +0000

    Logging the bot In

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,6 @@
 require('dotenv').config();
 
-console.log(process.env.DISCORDJS_BOT_TOKEN);
+const { Client } = require('discord.js');
+const client = new Client();
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
