fd4401ea5f623dc858c2e8e22136b25cb843ff9a
sserges <agounkeserge@gmail.com>
2020-09-01 11:29:11 +0000

    Kick Command

--- src/bot.js
+++ src/bot.js
@@ -16,8 +16,20 @@ client.on('message', (message) => {
       .substring(PREFIX.length)
       .split(/\s+/);
 
-    console.log(CMD_NAME);
-    console.log(args);
+    if (CMD_NAME === 'kick') {
+      if (!message.member.hasPermission('KICK_MEMBERS'))
+        return message.reply('You do not have permissions to use that command');
+      if (args.length === 0) return message.reply('Please provide an ID');
+      const member = message.guild.members.cache.get(args[0]);
+      if (member) {
+        member
+          .kick()
+          .then((member) => message.channel.send(`${member} was kicked.`))
+          .catch((err) => message.channel.send('I cannot kick that user:('));
+      } else {
+        message.channel.send('That member was not found');
+      }
+    }
   }
 });
 
